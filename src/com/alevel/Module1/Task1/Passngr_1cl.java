374e93668e5c64961bf22d3beb61558f24aef547
NaiFyyy <tahtoh@mail.ru>
2018-08-08 20:52:04 +0300

    DONE

---
+++ src/com/alevel/Module1/Task1/Passngr_1cl.java
@@ -0,0 +1,4 @@
+package com.alevel.Module1.Task1;
+
+public class Passngr_1cl {
+}
--- src/com/alevel/hometask/Exceptions/Main.java
+++ src/com/alevel/hometask/Exceptions/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
             System.out.println("Введите initial :");
             int initial = scanner.nextInt();
             System.out.println("Введите step:");
@@ -18,7 +17,5 @@ public class Main {
             } catch (ProgressionConfigurationException e) {
                 System.err.println(e.getMessage());
             }
-            break;
-        }
     }
 }
