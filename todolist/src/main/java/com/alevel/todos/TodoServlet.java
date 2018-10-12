9426dcb54d944c8b9329ef7939d14a2ed749a946
NaiFyyy <tahtoh@mail.ru>
2018-10-12 21:27:47 +0300

    ne

--- todolist/src/main/java/com/alevel/todos/TodoServlet.java
+++ todolist/src/main/java/com/alevel/todos/TodoServlet.java
@@ -1,4 +1,26 @@
 package com.alevel.todos;
 
-public class TodoServlet {
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/todo")
+public class TodoServlet extends HttpServlet {
+    private HikariDataSource dataSource;
+    private TodoRepository todoRepository;
+
+    @Override
+    public void init() throws ServletException {
+        HikariConfig hikariConfig = new HikariConfig("/hikary.properties");
+        dataSource=new HikariDataSource((hikariConfig));
+        todoRepository = new TodoRepository(dataSource);
+    }
+
+    @Override
+    public void destroy() {
+        dataSource.close();
+    }
 }
